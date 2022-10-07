eb4d5e572ec13f635bcdfa02ea71b04cddeda207
luketorv <104324965+luketorv@users.noreply.github.com>
2022-10-07 17:22:33 -0500

    added comment and reaction forms

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,47 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+      _id
+      username
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($commentText: String!) {
+    addComment(commentText: $commentText) {
+      _id
+      commentText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_REACTION = gql`
+  mutation addReaction($commentId: ID!, $reactionBody: String!) {
+    addReaction(commentId: $commentId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
