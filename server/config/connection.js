e896645c03ff8ec542b38b7147597347bedf847f
Sm4ck <mikeamartinez314@gmail.com>
2022-10-12 18:11:21 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config()
+console.log(process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/music-meetup2', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/music-meetup2', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
